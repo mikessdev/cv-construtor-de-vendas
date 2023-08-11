bbabba93c58aee1d6fb33c1e499f9b15b9a6c90e
mikessdev <mikesaints.2000@gmail.com>
2023-08-11 20:48:52 -0300

    filter component

---
+++ component/dropDown.js
@@ -0,0 +1,4 @@
+function toggleDropdown(element) {
+    const dropdownContent = document.getElementById(element);
+    dropdownContent.style.display = dropdownContent.style.display ===  "block" ?  "none" : "block";
+  }

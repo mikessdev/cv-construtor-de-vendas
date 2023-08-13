ab4692987f43f5dbf7a220c6fe214f35c32e697c
mikessdev <mikesaints.2000@gmail.com>
2023-08-13 19:32:08 -0300

    sort by  component

---
+++ data.js
@@ -0,0 +1,101 @@
+let enterpriseUnits = [
+    {
+        block: 'Maragogi',
+        numberRooms: '3',
+        floor: '3',
+        totalArea: '32,99 m2',
+        column: '15',
+        status: 'Disponível',
+        price: '300.000,00'
+    },
+    {
+        block: 'Trancoso',
+        numberRooms: '3',
+        floor: '3',
+        totalArea: '32,99 m2',
+        column: '12',
+        status: 'Disponível',
+        price: '350.000,00'
+    },
+    {
+        block: 'Noronha',
+        numberRooms: '2',
+        floor: '5',
+        totalArea: '54,58 m2',
+        column: '14',
+        status: 'Reservada',
+        price: '210.000,00'
+    },
+    {
+        block: 'Maragogi',
+        numberRooms: '2',
+        floor: '5',
+        totalArea: '32,99 m2',
+        column: '14',
+        status: 'Vendida',
+        price: '280.000,00'
+    },
+    {
+        block: 'Arraial do cabo',
+        numberRooms: '2',
+        floor: '6',
+        totalArea: '54,58 m2',
+        column: '13',
+        status: 'Disponível',
+        price: '270.000,00'
+    },
+    {
+        block: 'Carneiros',
+        numberRooms: '2',
+        floor: '2',
+        totalArea: '46,59 m2',
+        column: '16',
+        status: 'Vendida',
+        price: '310.000,00'
+    },
+    {
+        block: 'Maragogi',
+        numberRooms: '3',
+        floor: '7',
+        totalArea: '32,99 m2',
+        column: '14',
+        status: 'Reservada',
+        price: '343.000,00'
+    },
+    {
+        block: 'Trancoso',
+        numberRooms: '3',
+        floor: '8',
+        totalArea: '46,59 m2',
+        column: '16',
+        status: 'Vendida',
+        price: '180.000,00'
+    },
+    {
+        block: 'Maragogi',
+        numberRooms: '3',
+        floor: '7',
+        totalArea: '32,99 m2',
+        column: '14',
+        status: 'Reservada',
+        price: '313.000,00'
+    },
+    {
+        block: 'Trancoso',
+        numberRooms: '3',
+        floor: '8',
+        totalArea: '46,59 m2',
+        column: '16',
+        status: 'Reservada',
+        price: '298.000,00'
+    },
+    {
+        block: 'Trancoso',
+        numberRooms: '3',
+        floor: '8',
+        totalArea: '46,59 m2',
+        column: '16',
+        status: 'Bloqueada',
+        price: '298.000,00'
+    },
+];
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+//TODO: checar de onde vem a informação de status de venda.
+
+//Unidades Disponível
+let availableUnits;
+let blockedUnits;
+let soldUnits;
+let reservedUnits;
+
+const status = {
+    available: 'Disponível',
+    blocked: 'Bloqueada',
+    sold: 'Vendida',
+    reserved: 'Reservada'
+};
+
+function updateText(elementID, textcontent, auxText=''){
+    return document.getElementById(elementID).textContent = `${textcontent} ${auxText}`
+};
+
+availableUnits = enterpriseUnits.filter(e => e.status === status.available).length;
+
+updateText('available-units', availableUnits, 'Unidade Disponível');
+
+//Situação do Empreendiemnto
+
+updateText('enterprise-available', availableUnits);
+
+blockedUnits = enterpriseUnits.filter(e => e.status === status.blocked).length;
+
+updateText('enterprise-blocked', blockedUnits);
+
+soldUnits = enterpriseUnits.filter(e => e.status === status.sold).length;
+
+updateText('enterprise-sold', soldUnits);
+
+reservedUnits = enterpriseUnits.filter(e => e.status === status.reserved).length;
+
+updateText('enterprise-reserved', reservedUnits);
+
+//Filtrar Disponibilidade
+
+
+
+
 const getFiltersValue = () => {
     const ButtonText01 = document.getElementById('btn-01').textContent;
     const ButtonText02 = document.getElementById('btn-02').textContent;
